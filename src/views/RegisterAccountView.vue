fcd0993343220f631590d184f3a8a8cd6601722e
Schuyler Zheng <schuylerzheng@gmail.com>
2025-10-04 21:38:51 -0700

    Added ChatMessages Component & Register Component
    * Basic stuff on Register
    * Can't really start on chatmessages without firebase or a basic start page

---
+++ src/views/RegisterAccountView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  msg: string
+}>()
+
+const username = ref("Mr. Sir");
+</script>
+
+<template>
+  <div class="RegisterBox">
+    <h2>Register your account</h2>
+
+    <h4>Username:</h4>
+    <input v-model="username"/>
+
+    <h5>-- or --</h5>
+    <h4>Sign in with Google</h4>
+    <h1>Tmp until firebase</h1>
+  </div>
+</template>
+
+<style scoped>
+    .RegisterBox {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        border: 100px;
+    };
+</style>
