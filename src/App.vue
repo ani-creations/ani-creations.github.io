e702e221dfcf3ecd9abd3ef81b82c2bbf788b50b
Schuyler Zheng <schuylerzheng@gmail.com>
2025-10-05 14:49:18 -0700

    Updated Homescreen
    * Basically just removed everything that was only useful for the template.
    * The stuff could be bigger though

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome to AniCreations-V2" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/chat">Chat</RouterLink>
       </nav>
     </div>
   </header>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,9 +8,12 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      New version has:
+      <ul>
+        <li>Guest mode</li>
+        <li>Setting usernames</li>
+        <li>Just looks better</li>
+      </ul>
     </h3>
   </div>
 </template>
