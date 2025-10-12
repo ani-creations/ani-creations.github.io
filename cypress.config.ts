6e9ea80d4175a92a60a0e75fc5f13036174bbd42
Schuyler Zheng <schuylerzheng@gmail.com>
2025-10-12 16:44:46 -0700

    Cypress can retry 3 times before failing

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
 	projectId: '22iapc',
+	retries: 3,
 	e2e: {
 		specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
 		baseUrl: 'http://localhost:5173',
