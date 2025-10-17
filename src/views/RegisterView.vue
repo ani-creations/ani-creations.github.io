69dc54aaacff00cdc23501703e055c060b47e0d9
SZ <54783078+Schuylerzheng@users.noreply.github.com>
2025-10-16 22:00:55 -0700

    Revert "Cypress Port Switch"

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -15,7 +15,7 @@ function signInWithGoogle() {
 			// user is signed in here
 			// auth.onAuthStateChanged will handle UI update, so no need to do it here
 
-			alert('Google Sign-In Success: ' + result.user);
+			console.log('Google Sign-In Success:', result.user);
 		})
 		.catch((error) => {
 			console.error('Google Sign-In Error:', error.message)
@@ -25,7 +25,7 @@ function signInWithGoogle() {
 </script>
 
 <template>
-	<main>
+	<div class="RegisterBox">
 		<h1>Register your account</h1>
 
 		<h4>Username:</h4>
@@ -33,12 +33,13 @@ function signInWithGoogle() {
 
 		<h2 class="divider">-- or --</h2>
 
-		<button @click="signInWithGoogle"><img class="coolImg" src="https://www.gstatic.com/marketing-cms/assets/images/d5/dc/cfe9ce8b4425b410b49b7f2dd3f3/g.webp=s96-fcrop64=1,00000000ffffffff-rw"/>        Sign in with Google</button>
-	</main>
+		<h4>Sign in with Google</h4>
+		<button @click="signInWithGoogle">Sign in with Google</button>
+	</div>
 </template>
 
 <style scoped>
-main {
+.RegisterBox {
 	align-items: center;
 	text-align: center;
 	align-content: center;
@@ -48,18 +49,5 @@ main {
 .divider {
 	margin-top: 100px;
 	margin-bottom: 100px;
-}
-
-.coolImg {
-	width: 50px;
-	height: 50px;
-}
-
-button {
-	padding: 10px;
-	font-size: 40px;
-	cursor: pointer;
-	size: 1000px;
-	width: 80rem;
 }
 </style>
